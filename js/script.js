18b8d0d845826d900b1ec0a294f507f9dc8be53e
AkromCik <@akromjonrahimjon712>
2022-06-08 08:34:07 +0500

    add new pages

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
-let btn =  document.querySelector('#btn');
-let menu = document.querySelector('.menu');
+let btn = document.querySelector("#btn");
+let menu = document.querySelector(".menu");
 
-btn.addEventListener('click', ()=>{
-    btn.classList.toggle('fa-times');
-    menu.classList.toggle('active');
-})
+btn.addEventListener("click", () => {
+  btn.classList.toggle("fa-times");
+  menu.classList.toggle("active");
+});
+
+const accordion = document.querySelector(".faqs__accordionBx");
+
+for (i = 0; i < accordion; i++) {
+  accordion[i].addEventListener("click", function () {
+    
+  });
+}
